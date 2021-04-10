22b0e69eefe28098c2971d23f2aa4ce5b6d89b21
AhmedQuas <ahmed.quas@gmail.com>
2021-04-10 13:25:49 +0200

    Path parameters and API documentation

--- pythonFastAPI/app/main.py
+++ pythonFastAPI/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+#API documentation is accessible by /doc or /redoc path
 app = FastAPI()
 
 @app.get('/')
@@ -9,3 +10,20 @@ def index():
 @app.get('/about')
 def about():
     return {'data':{'about page'}}
+
+#Path are processing from top to down
+@app.get('/blog/unpublished')
+def show_unpublished_posts():
+    return {'data':{'unpublished'}}
+
+@app.get('/blog/{id}')
+def show_blog_post(id: int):
+    #fetch posts with given id
+    return {'data':{'post_id':id}}
+
+
+@app.get('/blog/{id}/comments')
+def show_blog_post_comments(id: str):
+    #fetch comments from given post
+    return {'data':{'1','2'}}
+    
